db094960a4a4aad742c18df87081972ad8f6cdd2
Allan <allan.agomesp@gmail.com>
2024-03-24 11:26:53 -0300

    fix: style full web app

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,7 +2,7 @@ import Balance from "../Components/Balance.tsx";
 
 function Root() {
     return (
-        <div className="m-96">
+        <div className="flex items-center justify-center w-full h-full">
             <Balance/>
         </div>
     );
